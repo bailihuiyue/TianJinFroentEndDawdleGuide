0248ce31246a116cbab08d922de17e5743a8737f
jack.li1 <jack.li1@homecreditcfc.cn>
2021-07-02 17:21:51 +0800

    Update watcher.js

--- 前端架构/MVVM/watcher.js
+++ 前端架构/MVVM/watcher.js
@@ -22,6 +22,7 @@ function Watcher(vm, exp, fn) {
   let val = vm.$data;
   let arr = exp.split('.');
   arr.forEach(function (k) {
+    // 这句目的只是为了循环调出val[k]的值然后调用Object.defineProperty的get(){}然后addSub
       val = val[k];
   })
   Dep.target = null; // 保证watcher不会重复添加
@@ -29,6 +30,7 @@ function Watcher(vm, exp, fn) {
 Watcher.prototype.update = function() {
   let val = this.vm.$data;
   let arr = this.exp.split('.');
+  // 这句目跟上面的不一样,这句是为了获取输入input的值,然后传给this.fn
   arr.forEach(function (k) {
       val = val[k];
   })
