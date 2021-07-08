4f128949d7ae11f4f1f48506815c510be20f33c8
jack.li1 <jack.li1@homecreditcfc.cn>
2021-07-08 14:40:54 +0800

    test

--- 前端架构/MVVM/MVVM.js
+++ 前端架构/MVVM/MVVM.js
@@ -1,13 +1,13 @@
 class MVVM {
-    constructor(options) {
-        // 当new该类时，参数就会传到构造函数中 options就是el  data computed ...
-        this.$el = options.el; // 创建一个当前实例$el
-        this.$data = options.data;
-        // 判断根元素是否存在 <div id='app'></div> =>  编译模板
-        if (this.$el) {
-            // 把data里的数据 全部转化成用Object.defineProperty来定义
-            new Observer(this.$data);
-            new Compiler(this.$el, this);
-        }
+  constructor(options) {
+    // 当new该类时，参数就会传到构造函数中 options就是el  data computed ...
+    this.$el = options.el; // 创建一个当前实例$el
+    this.$data = options.data;
+    // 判断根元素是否存在 <div id='app'></div> =>  编译模板
+    if (this.$el) {
+      // 把data里的数据 全部转化成用Object.defineProperty来定义
+      new Observer(this.$data);
+      new Compiler(this.$el, this);
     }
+  }
 }
